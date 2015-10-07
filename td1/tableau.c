df1377a101a8d2d36ca6e6981bda7c84d6a636ba
fcdg-uvsq <fdesgraviers@yahoo.fr>
2015-10-07 22:43:27 +0200

    màj tableau.c/td1

--- td1/tableau.c
+++ td1/tableau.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 void initialise (int *tableau, int taille);
+void affiche    (int *tableau, int taille);
 int  produit    (int *tableau, int taille);
 int  minimum    (int *tableau, int taille);
 void decalage   (int *tableau, int taille);
@@ -8,14 +9,17 @@ void decalage   (int *tableau, int taille);
 
 int main()
 {
-	int taille = 3; //100;
+	int taille = 6;
+	int tableau[] = { 3, 7, 12, 9, 1, 4};
 	
-	int tableau[] = { 3, 7, 5};
 	
-	printf("Element du tableau : %d %d %d\n", tableau[0], tableau[1], tableau[2]);
+	affiche(tableau, taille);
 	
 	printf("Produit des éléments du tableau : %d\n", produit(tableau, taille));
-	printf("Minimum du tableau = %d", minimum(tableau, taille));
+	printf("Minimum du tableau = %d\n", minimum(tableau, taille));
+	printf("Effet du décalage : ");
+	decalage(tableau, taille);
+	affiche(tableau, taille);
 	
 
 	return 0;
@@ -29,6 +33,17 @@ void initialise(int *tableau, int taille)
 	{
 		tableau[i] = 1;
 	}	
+}
+
+void affiche(int *tableau, int taille)
+{
+	int i;
+	//char message[] = "Eléments du tableau : "; puts(message);
+	for(i=0; i<taille; i++)
+	{
+		printf("%d\t", tableau[i]);
+	}
+	putchar('\n');	
 }
 
 
@@ -47,20 +62,30 @@ int produit(int *tableau, int taille)
 
 int minimum (int *tableau, int taille)
 {
-	int i;
+	int i = 1;
 	int min = tableau[0];
 	
-	for(i=1; i<taille; i++)
+	while (i<taille)
 	{
 		if (min > tableau[i])
 		{
-			min = i;
-		}	
+			min = tableau[i];
+		}
+		i++;
 	}
 	return min;
 }
 
+
 void decalage (int *tableau, int taille)
 {
+	tableau[0] = 0;
+	tableau[taille] = -1;
 	
+	int i;
+	
+	for(i=1; i<=taille-1; i++) // On commence à 1 car la 1re case (tableau[0]) vaut déjà 0 et la dernière est supprimée (-1)
+	{
+		tableau[i] = tableau[i+1];		
+	}
 }
